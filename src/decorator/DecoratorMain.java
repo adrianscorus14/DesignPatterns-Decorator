3f9b18124f0ed836a00e4f8b6ee9771c8f2fe1e2
adrianscorus14 <adrianscorus14@gmail.com>
2020-10-18 17:33:34 +0300

    Initial commit

---
+++ src/decorator/DecoratorMain.java
@@ -0,0 +1,22 @@
+package decorator;
+
+import decorator.example.BasicPizza;
+import decorator.example.HamPizza;
+import decorator.example.MushroomsPizza;
+import decorator.example.Pizza;
+
+public class DecoratorMain {
+
+    public static void main(String[] args) {
+        Pizza pizza = new BasicPizza();
+        pizza.printIngredients();
+        Pizza hamPizza = new HamPizza(pizza);
+        pizza.printIngredients();
+        Pizza mushroomsPizza = new MushroomsPizza(pizza);
+        pizza.printIngredients();
+
+//        Pizza seafoodPizza = new SeafoodPizza(new BasicPizza());
+//        pizza.printIngredients();
+    }
+
+}
